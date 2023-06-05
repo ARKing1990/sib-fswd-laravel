211b22641d0817a54b488c08a4316826a1b2ff7e
Husein Syahfikri <20200410700019@student.umj.ac.id>
2023-06-05 11:25:44 +0700

    [FIX]Fix create user baru'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
